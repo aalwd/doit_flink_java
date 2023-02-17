ee835020f90bc4a4ac80b8ed8b9d239a51b9db44
nhd <1823781434@qq.com>
2023-02-17 11:41:28 +0800

    Initial commit

---
+++ src/scala/com/doit/flink/day01/StreamWordCount.scala
@@ -0,0 +1,23 @@
+package com.doit.flink.day01
+
+object StreamWordCount {
+
+  def main(args: Array[String]): Unit = {
+    /**
+     * 创建spark的程序
+     * 1 sparkcontext
+     * 2. rdd
+     * 3. rdd. transformation
+     * 4. action
+     * 5. release resources
+     */
+
+
+    // 创建flink 的执行环境
+
+    // 调用evn的source方法创建datastream(Flink的抽象数据集)
+
+    //
+  }
+
+}
